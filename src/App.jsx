e4053cb0e4e17f5b1964d1b68b12c7167f3c7335
nikotastic12 <velasqueznikol10@gmail.com>
2025-07-16 15:44:34 -0500

    Version 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import ErrorPage from './pages/ErrorPage'
+import Dashboard from './pages/Dashboard'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
